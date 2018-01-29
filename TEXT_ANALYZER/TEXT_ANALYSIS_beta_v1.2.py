48635ca533a8cd994aaccfb37c4f01b98acd3dbd
Eric Gemmell <8333775+palamalama@users.noreply.github.com>
2018-01-29 15:11:21 +0000

    Split Pronoun Remover from Text Analysis

---
+++ TEXT_ANALYZER/TEXT_ANALYSIS_beta_v1.2.py
@@ -0,0 +1,10 @@
+import PronounRemover
+
+print("Welcolme to This Version of Text_Analysis...\n")
+print("Please Input the Text You Would Like To Analyse\n")
+text = raw_input("")
+
+text = PronounRemover.Process(text)
+
+print("Here's what we got")
+print text
